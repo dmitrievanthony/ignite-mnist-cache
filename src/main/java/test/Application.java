87396a9873806e61530b4be1044c616441732991
Anton Dmitriev <dmitrievanthony@gmail.com>
2018-08-03 12:56:35 +0300

    Update

--- src/main/java/test/Application.java
+++ src/main/java/test/Application.java
@@ -9,53 +9,77 @@ import org.apache.ignite.Ignition;
 import org.apache.ignite.cache.affinity.rendezvous.RendezvousAffinityFunction;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import picocli.CommandLine;
 
 public class Application {
 
-    public static void main(String... args) throws InterruptedException, IOException {
-        IgniteConfiguration cfg = new IgniteConfiguration();
-        cfg.setClientMode(false);
-
-        try (Ignite ignite = Ignition.start(cfg)) {
-            CacheConfiguration<Integer, LabeledImage> cacheCfg = new CacheConfiguration<>();
-            cacheCfg.setName("MNIST_CACHE");
-            cacheCfg.setAffinity(new RendezvousAffinityFunction(false, 10));
-
-            IgniteCache<Integer, LabeledImage> cache = ignite.createCache(cacheCfg);
-
-            System.out.println("Filling cache...");
-            fillCache(cache);
-            System.out.println("Cache is ready");
-
-            Thread.currentThread().join();
-        }
+    public static void main(String... args) {
+        CommandLine.run(new Command(), System.out, args);
     }
 
-    private static void fillCache(IgniteCache<Integer, LabeledImage> cache) throws IOException {
-        DataInputStream images = new DataInputStream(Application.class.getClassLoader()
-            .getResourceAsStream("test/train-images-idx3-ubyte"));
-        DataInputStream labels = new DataInputStream(Application.class.getClassLoader()
-            .getResourceAsStream("test/train-labels-idx1-ubyte"));
+    @CommandLine.Command()
+    private static class Command implements Runnable {
 
-        images.readInt();
-        int imagesLen = images.readInt();
-        int imagesRows = images.readInt();
-        int imagesCols = images.readInt();
+        @CommandLine.Option(names = {"-c", "--config"}, description = "Apache Ignite configuration")
+        private String cfg;
 
-        labels.readInt();
-        int labelsLen = labels.readInt();
+        @Override public void run() {
+            try (Ignite ignite = getIgnite()) {
+                if (ignite.cacheNames().contains("MNIST_CACHE"))
+                    System.out.println("Apache Ignite cluster already contains MNIST_CACHE");
+                else {
+                    CacheConfiguration<Integer, LabeledImage> cacheCfg = new CacheConfiguration<>();
+                    cacheCfg.setName("MNIST_CACHE");
+                    cacheCfg.setAffinity(new RendezvousAffinityFunction(false, 8));
 
-        if (imagesLen != labelsLen)
-            throw new IllegalStateException("Number of images and labels are not equal");
+                    IgniteCache<Integer, LabeledImage> cache = ignite.getOrCreateCache(cacheCfg);
+                    fillCache(cache);
 
-        for (int i = 0; i < imagesLen; i++) {
-            double[] pixels = new double[imagesRows * imagesCols];
-            for (int j = 0; j < imagesRows * imagesCols; j++) {
-                byte px = images.readByte();
-                pixels[j] = (px + 128) / 255;
+                    System.out.println("MNIST_CACHE created");
+                }
             }
-            int lb = labels.readByte();
-            cache.put(i, new LabeledImage(pixels, lb));
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private Ignite getIgnite() {
+            if (cfg != null)
+                return Ignition.start(cfg);
+            else {
+                IgniteConfiguration cfg = new IgniteConfiguration();
+                cfg.setClientMode(true);
+
+                return Ignition.start(cfg);
+            }
+        }
+
+        private void fillCache(IgniteCache<Integer, LabeledImage> cache) throws IOException {
+            DataInputStream images = new DataInputStream(Application.class.getClassLoader()
+                .getResourceAsStream("test/train-images-idx3-ubyte"));
+            DataInputStream labels = new DataInputStream(Application.class.getClassLoader()
+                .getResourceAsStream("test/train-labels-idx1-ubyte"));
+
+            images.readInt();
+            int imagesLen = images.readInt();
+            int imagesRows = images.readInt();
+            int imagesCols = images.readInt();
+
+            labels.readInt();
+            int labelsLen = labels.readInt();
+
+            if (imagesLen != labelsLen)
+                throw new IllegalStateException("Number of images and labels are not equal");
+
+            for (int i = 0; i < imagesLen; i++) {
+                double[] pixels = new double[imagesRows * imagesCols];
+                for (int j = 0; j < imagesRows * imagesCols; j++) {
+                    byte px = images.readByte();
+                    pixels[j] = (px + 128) / 255;
+                }
+                int lb = labels.readByte();
+                cache.put(i, new LabeledImage(pixels, lb));
+            }
         }
     }
 
